83c348965cf8efa644cfe3f384cd7eaf02bfef17
Shamira <marshallscm@gmail.com>
2019-04-21 11:17:35 -0700

    Added edge case and Big O explanation.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,14 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: ? O(n) - linear with n being the number input.
+# Space complexity: ? O(n) with n being the number input for the factorial.
 def factorial(number)
-  raise NotImplementedError
+  if number == nil || number < 0
+    raise ArgumentError, "Need positive integer"
+  end
+  factorial = 1
+  while number >= 1
+    factorial *= number
+    number = number - 1
+  end
+  return factorial
 end
